3899263331d4efdf0461f40d3da346056af8342f
Jeffimenson <jeffengim@gmail.com>
2023-08-06 22:21:50 -0400

    Make about page loader and make minor additions elsewhere in the dom generation code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 
 import logo from './bcbonalds.jpg';
+import manEatingBurger from './man_eating_burger.jpeg';
 
 const body = document.querySelector('body');
 const content = document.querySelector('#content');
@@ -13,13 +14,14 @@ function createHeader(){
     const header = document.createElement('header');
     const logoImg = document.createElement('img');
     logoImg.src = logo;
+    logoImg.alt = 'logo';
     logoImg.classList.add('logo');
-    header.appendChild(logoImg);
+    header.append(logoImg);
 
     for (let i = 0; i < 3; i++){
         const newButton = document.createElement('button');
         newButton.classList.add('fake-link');
-        header.appendChild(newButton);
+        header.append(newButton);
         fakeLinks[i] = newButton;
     }
     fakeLinks[0].setAttribute('id', 'menu');
@@ -32,9 +34,9 @@ function createHeader(){
     const orderButton = document.createElement('button');
     orderButton.setAttribute('id', 'order');
     orderButton.textContent = 'Order now';
-    header.appendChild(orderButton);
+    header.append(orderButton);
 
-    body.appendChild(header);
+    body.prepend(header);
 }
 
 function loadAbout(aboutFakeLink, fakeLinks) {
@@ -42,6 +44,20 @@ function loadAbout(aboutFakeLink, fakeLinks) {
         fakeLink.classList.remove('selected');
     }));
     aboutFakeLink.classList.add('selected');
+
+    const heading1 = document.createElement('h1');
+    heading1.textContent = "About us";
+    content.append(heading1);
+    const img = document.createElement('img');
+    img.src = manEatingBurger;
+    img.alt = "man enjoying bcbonalds burger";
+    content.append(img);
+    const heading2 = document.createElement('h2');
+    heading2.textContent = "Dedication to you";
+    content.append(heading2);
+    const para = document.createElement('p');
+    para.textContent = "BcBonalds, where we redefine fast food with imaginative flavors, a quirky atmosphere, and a mission to make dining fun and unforgettable! Join us for a delightful experience that leaves you smiling with every bite.";
+    content.append(para);
 }
 
 /* make this here now lol
